8ef69aa57250e58aa629112f08fcfc3fe882a7d7
aknodell <aknodell@trinity.edu>
2016-11-18 14:47:17 -0600

    Add files via upload

---
+++ PracticeChromeExtension/popup.js
@@ -0,0 +1,14 @@
+console.log('extension running');
+
+// var script = document.createElement('script');
+// script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js';
+// script.type = 'text/javascript';
+// document.getElementsByTagName('head')[0].appendChild(script);
+
+var width = '650px'
+
+document.body.style.setProperty("padding-right",width,"important");
+var personal = document.getElementById('p-personal');
+personal.style.setProperty("padding-right",width,"important");
+var head = document.getElementById('mw-head');
+head.style.setProperty("padding-right",width,"important");
