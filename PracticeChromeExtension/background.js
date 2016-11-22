8c0328540652b1089210c67695a6ab754981f3d0
aknodell <aknodell@trinity.edu>
2016-11-22 02:30:27 -0600

    Add files via upload
    Functional knowledge graph

--- PracticeChromeExtension/background.js
+++ PracticeChromeExtension/background.js
@@ -1,4 +1,46 @@
-function getPageTitle() {
-	var pageTitle = document.getElementById('firstHeading').innerHTML;
-	return pageTitle;
-}
+// function getPageTitle() {
+	// var pageTitle = document.getElementById('firstHeading').innerHTML;
+	// return pageTitle;
+// }
+
+var pages = [];
+// var knowledgeGraph = {nodes: [], edges: []};
+var previousPage = null;
+var graph = new Springy.Graph();
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        var pageInfo = JSON.parse(request);
+        
+        var currentPage;
+        var dupeNode = false;
+        for (i = 0; i < graph.nodes.length; i++) {
+            if (graph.nodes[i].data.label == pageInfo.title) {
+                currentPage = graph.nodes[i];
+                dupeNode = true;
+            }
+        }
+        
+        if (!dupeNode)
+            currentPage = graph.newNode({label: pageInfo.title, url: pageInfo.url});
+        
+        var invalidEdge = false;
+
+        if (previousPage == null)
+            invalidEdge = true;
+        else if (previousPage.data.label == currentPage.data.label)
+            invalidEdge = true;
+        
+        for (i = 0; i < graph.edges.length; i++)
+            if ((graph.edges[i].source.data.label == previousPage.data.label) 
+                    && (graph.edges[i].target.data.label == currentPage.data.label))
+                invalidEdge = true;
+        
+        if (!invalidEdge)
+            graph.newEdge(previousPage, currentPage);            
+
+        previousPage = currentPage;      
+          
+        sendResponse(graph);
+});
+
--- PracticeChromeExtension/popup.js
+++ PracticeChromeExtension/popup.js
@@ -7,10 +7,44 @@ document.getElementById('content').style.setProperty("margin-right",'1em',"impor
 document.getElementById('footer').style.setProperty("padding-right",width,"important");
 document.getElementById('footer').style.setProperty("margin-right",'1em',"important");
 
-var div = document.createElement("div");
-document.body.appendChild(div);
-div.className = "extension";
-var para = document.createElement('p');
-var bgPage = chrome.extension.getBackgroundPage(); // not working right now http://stackoverflow.com/questions/13777887/call-background-function-of-chrome-extension-from-a-site
-para.innerHTML = bgPage.getPageTitle();
-div.appendChild(para);
+var pageInformation = {title:"", url:""};
+var ext = document.createElement("div");
+document.body.appendChild(ext);
+ext.id = "extension";
+ext.className = "extension";
+var kGraph = document.createElement("div");
+ext.appendChild(kGraph);
+kGraph.id = "knowledgeGraph";
+kGraph.className = "knowledgeGraph";
+kGraph.innerHTML = '<canvas id="springydemo" class="canvas" width="650" height="480" />';
+
+pageInformation.title = document.getElementById('firstHeading').innerHTML;
+pageInformation.url = window.location.href;
+
+chrome.runtime.sendMessage(JSON.stringify(pageInformation), function(response) {
+    var graph = new Springy.Graph();
+    
+    for (i = 0; i < response.nodes.length; i++) {
+        graph.newNode(response.nodes[i].data);
+        (function () {
+            var currentIndex = i;
+            var node = graph.nodes[currentIndex];
+            node.data.ondoubleclick = 
+                    function() { document.location.href = node.data.url; };            
+        })();
+    }
+    
+    for (i = 0; i < response.edges.length; i++) {
+        graph.newEdge(graph.nodes[response.edges[i].source.id],
+                graph.nodes[response.edges[i].target.id]);
+    }
+
+    jQuery(function(){
+      var springy = window.springy = jQuery('#springydemo').springy({
+        graph: graph,
+        nodeSelected: function(node){
+          console.log(node.data.url);
+        }
+      });
+    });    
+});
