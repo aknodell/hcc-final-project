c65b7e458e92065e30c0508b89a3af8ebb757083
aknodell <aknodell@trinity.edu>
2016-11-23 02:00:18 -0600

    Add files via upload

--- PracticeChromeExtension/background.js
+++ PracticeChromeExtension/background.js
@@ -7,40 +7,51 @@ var pages = [];
 // var knowledgeGraph = {nodes: [], edges: []};
 var previousPage = null;
 var graph = new Springy.Graph();
+var annotations = {};
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        var pageInfo = JSON.parse(request);
-        
-        var currentPage;
-        var dupeNode = false;
-        for (i = 0; i < graph.nodes.length; i++) {
-            if (graph.nodes[i].data.label == pageInfo.title) {
-                currentPage = graph.nodes[i];
-                dupeNode = true;
+        if (request.message == "load") {
+            var pageInfo = JSON.parse(request.pageInformation);
+            
+            var currentPage;
+            var dupeNode = false;
+            for (i = 0; i < graph.nodes.length; i++) {
+                if (graph.nodes[i].data.label == pageInfo.title) {
+                    currentPage = graph.nodes[i];
+                    dupeNode = true;
+                }
             }
-        }
-        
-        if (!dupeNode)
-            currentPage = graph.newNode({label: pageInfo.title, url: pageInfo.url});
-        
-        var invalidEdge = false;
+            
+            if (!dupeNode)
+                currentPage = graph.newNode({label: pageInfo.title, url: pageInfo.url});
+            
+            var invalidEdge = false;
 
-        if (previousPage == null)
-            invalidEdge = true;
-        else if (previousPage.data.label == currentPage.data.label)
-            invalidEdge = true;
-        
-        for (i = 0; i < graph.edges.length; i++)
-            if ((graph.edges[i].source.data.label == previousPage.data.label) 
-                    && (graph.edges[i].target.data.label == currentPage.data.label))
+            if (previousPage == null)
                 invalidEdge = true;
-        
-        if (!invalidEdge)
-            graph.newEdge(previousPage, currentPage);            
+            else if (previousPage.data.label == currentPage.data.label)
+                invalidEdge = true;
+            
+            for (i = 0; i < graph.edges.length; i++)
+                if ((graph.edges[i].source.data.label == previousPage.data.label) 
+                        && (graph.edges[i].target.data.label == currentPage.data.label))
+                    invalidEdge = true;
+            
+            if (!invalidEdge)
+                graph.newEdge(previousPage, currentPage);            
+            
+            if (annotations[pageInfo.title] == null)
+                annotations[pageInfo.title] = [];
 
-        previousPage = currentPage;      
-          
-        sendResponse(graph);
+            previousPage = currentPage;      
+              
+            sendResponse({kGraph: graph, anns: annotations[pageInfo.title]});
+        }
+        else if (request.message == "addAnnotation")
+        {
+            annotations[request.key].push({text:request.annotate, note:"some note"});
+            sendResponse({message:"success", annos:annotations[request.key]});
+        }
 });
 
--- PracticeChromeExtension/popup.js
+++ PracticeChromeExtension/popup.js
@@ -1,4 +1,5 @@
 var width = '650px'
+annoText = "";
 
 var paddingElems = ['p-personal', 'mw-head', 'content', 'footer'];
 var marginElems = ['content', 'footer'];
@@ -10,47 +11,109 @@ for (elem in marginElems)
     document.getElementById(marginElems[elem]).style.setProperty("margin-right",'1em',"important");
 
 var pageInformation = {title:"", url:""};
+
 var ext = document.createElement("div");
 document.body.appendChild(ext);
 ext.id = "extension";
 ext.className = "extension";
+
 var kGraph = document.createElement("div");
 ext.appendChild(kGraph);
 kGraph.id = "knowledge-graph";
 kGraph.className = "knowledge-graph";
 kGraph.innerHTML = '<canvas id="springydemo" class="canvas" width="610" height="420" />';
+
 var anno = document.createElement("div");
 ext.appendChild(anno);
 anno.id = "annotations";
 anno.className = "annotations";
 
+var addAnnotationSpan = document.createElement("span");
+addAnnotationSpan.id = "add-annotation";
+addAnnotationSpan.className = "add-annotation";
+addAnnotationSpan.innerHTML = "Add Annotation";
+addAnnotationSpan.onclick = function() { addAnnotation() };
+
+document.body.appendChild(addAnnotationSpan);
+
 pageInformation.title = document.getElementById('firstHeading').innerHTML;
 pageInformation.url = window.location.href;
 
-chrome.runtime.sendMessage(JSON.stringify(pageInformation), function(response) {
-    var graph = new Springy.Graph();
-    
-    for (i = 0; i < response.nodes.length; i++) {
-        graph.newNode(response.nodes[i].data);
-        (function () {
-            var currentIndex = i;
-            var node = graph.nodes[currentIndex];
-            node.data.ondoubleclick = 
-                    function() { document.location.href = node.data.url; };            
-        })();
-    }
-    
-    for (i = 0; i < response.edges.length; i++) {
-        graph.newEdge(graph.nodes[response.edges[i].source.id],
-                graph.nodes[response.edges[i].target.id]);
-    }
-
-    jQuery(function(){
-      var springy = window.springy = jQuery('#springydemo').springy({
-        graph: graph,
-        nodeSelected: function(node){
-          console.log(node.data.url);
+window.onload = function() {
+    chrome.runtime.sendMessage({message:"load", pageInformation: JSON.stringify(pageInformation)}, function(response) {
+        var graph = new Springy.Graph();
+        
+        for (i = 0; i < response.kGraph.nodes.length; i++) {
+            graph.newNode(response.kGraph.nodes[i].data);
+            (function () {
+                var currentIndex = i;
+                var node = graph.nodes[currentIndex];
+                node.data.ondoubleclick = 
+                        function() { document.location.href = node.data.url; };            
+            })();
         }
-      });
-    });    
-});
+        
+        for (i = 0; i < response.kGraph.edges.length; i++) {
+            graph.newEdge(graph.nodes[response.kGraph.edges[i].source.id],
+                    graph.nodes[response.kGraph.edges[i].target.id]);
+        }
+        
+        for (i = 0; i < response.anns.length; i++) {
+            var newAnnotation = document.createElement("div");
+            var par = document.createElement("p");
+            par.innerHTML = response.anns[i].text;
+            newAnnotation.appendChild(par);
+            anno.appendChild(par);              
+        }
+        
+        console.log(response.anns);
+
+        jQuery(function(){
+          var springy = window.springy = jQuery('#springydemo').springy({
+            graph: graph,
+            nodeSelected: function(node){
+              // console.log(node.data.url);
+            }
+          });
+        });    
+    });
+}
+
+document.onmouseup = function(event) {
+    if (addAnnotationSpan.style.visibility == "visible") {
+        addAnnotationSpan.style.visibility = "hidden";
+    } else {
+        window.annoText = getSelectionText();
+        if (window.annoText != "") {
+            addAnnotationSpan.style.visibility = "visible";
+            addAnnotationSpan.style.top = event.pageY + "px";
+            addAnnotationSpan.style.left = event.pageX + "px";
+        }        
+    }
+};
+
+function addAnnotation() {
+   if (window.annoText != "") {
+        var newAnnotation = document.createElement("div");
+        var par = document.createElement("p");
+        par.innerHTML = window.annoText;
+        newAnnotation.appendChild(par);
+        anno.appendChild(par);
+        
+        chrome.runtime.sendMessage({message:"addAnnotation", annotate:window.annoText, key:pageInformation.title}, function(response) {
+            console.log(response);
+        });
+    }
+}
+
+function getSelectionText() {
+    var selText = "";
+    if (window.getSelection) {
+        selText = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        selText = document.selection.createRange().text;
+    }
+    return selText;
+}
+
+
